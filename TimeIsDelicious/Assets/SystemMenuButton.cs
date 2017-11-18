89d06c812b7f2853acbd7dc50d6df5b391d135e9
shisoromi <over.hrk@gmail.com>
2017-11-19 01:47:24 +0900

    Create basic button motion

---
+++ TimeIsDelicious/Assets/SystemMenuButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SystemMenuButton : MonoBehaviour {
+
+    private Animator animator;
+
+    // Use this for initialization
+    void Start () {
+        animator = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	//void Update () {
+		
+	//}
+
+    
+
+    public void OnClick()
+    {
+        Debug.Log("Clicked/SystemMenuButton");
+        //animator.SetBool("IsSystemMenuButtonSelected", true);
+    }
+}
