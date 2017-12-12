4811c9a1e517b7f10f69ad45bd5e7976167532fd
sryoji <sryoji5087@gmail.com>
2017-12-12 22:34:10 +0900

    add game start scene

---
+++ TimeIsDelicious/Assets/GameStart/LogoController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogoController : MonoBehaviour {
+
+	public GameObject title;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void EndFadeOutAnimation () {
+		title.GetComponent<TitleController> ().Show ();
+	}
+}
