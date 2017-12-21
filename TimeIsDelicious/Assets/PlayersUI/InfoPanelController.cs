7afec41e126614e664623c3f95f4f3b2603a3629
sryoji <sryoji5087@gmail.com>
2017-12-21 23:42:03 +0900

    ラウンド、ターン情報を表示

--- TimeIsDelicious/Assets/PlayersUI/InfoPanelController.cs
+++ TimeIsDelicious/Assets/PlayersUI/InfoPanelController.cs
@@ -1,20 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InfoPanelController : MonoBehaviour {
 
 	public GameObject RoundTextObj;
 	public GameObject TurnTextObj;
 
+	private Text _roundText;
+	private Text _turnText;
+
+	private GameDirector _gd;
+
 	// Use this for initialization
 	void Start () {
-		
+		_roundText = RoundTextObj.GetComponent<Text> ();
+		_turnText = TurnTextObj.GetComponent<Text> ();
+
+		_gd = GameObject.Find("GameDirector").GetComponent<GameDirector>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		// いまのターン数とか見て随時更新？
+		_roundText.text = "Round   " +  _gd.RoundCount.ToString() + " / 3";
+		_turnText.text  = "Turn     " + _gd.TurnCount.ToString() + " / 10";
+
 	}
 }
