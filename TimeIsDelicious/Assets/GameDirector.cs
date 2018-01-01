2302c49c1256d3ea19d0182d8c07ab53606900a8
over-hrk <over.hrk@gmail.com>
2018-01-02 02:30:21 +0900

    Refactoring GameDirector

--- TimeIsDelicious/Assets/GameDirector.cs
+++ TimeIsDelicious/Assets/GameDirector.cs
@@ -2,18 +2,17 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using UnityEngine;
+using UniRx;
 
 // UIからの通知はこのViewが受け取る(動的に表示される「はい」「いいえ」ボタンやシステムボタンなど)
 public class GameDirector : MonoBehaviour
 {
-    private GameDirectorVM _mainVM; // ゲーム全体のView Model
-
     private GameObject playerUIWindow;
 
     public int TurnCount { get; set; }
     public int RoundCount { get; set; }
     public MainModel.Status Status { get; set; }
-    public string CurrentPlayerName { get; private set; }
+    public string CurrentPlayerName { get; set; }
 
     // Use this for initialization
     void Start()
@@ -22,47 +21,8 @@ public class GameDirector : MonoBehaviour
         TurnCount = 0;
         RoundCount = 0;
 
-        
-        _mainVM = new GameDirectorVM(this);
-
         // 監視対象GameObjectを取得
         playerUIWindow = GameObject.Find("PlayerUIPanel");
-
-        // ゲームを開始する
-        var pObj = GameObject.Find("PermanentObj")?.GetComponent<PermanentObj>();
-        int? numOfPlayers = pObj?.playerNum;
-        _mainVM.StartTimeIsDelicious( numOfPlayers.HasValue ? numOfPlayers.Value : 4);
-    }
-
-
-    public void GameRoundStart()
-    {
-        StartCoroutine(RoundStart());
-    }
-
-    public void SetCurrentPlayer(string Name)
-    {
-        CurrentPlayerName = Name;
-    }
-
-    public void RaisePopUp(string Name)
-    {
-        popupWindow.GetComponent<PopupMessaegController>().Popup(Name + "さんは肉を選んでください。");
-    }
-
-    private IEnumerator RoundStart()
-    {
-        // PlyerUIWindowが準備完了したら
-        yield return new WaitUntil(() =>
-        {
-            return playerUIWindow.GetComponent<PlayersUIWindowController>().UIRready;
-        });
-
-        // メッセージを表示して、確認されたらStartRound
-        popupWindow.GetComponent<PopupMessaegController>().Popup("ラウンド開始します", () =>
-        {
-            _mainVM.StartRound();
-        });
     }
 
     // Update is called once per frame
@@ -76,63 +36,35 @@ public class GameDirector : MonoBehaviour
     private DiceController dc;
     public void OnClickForDebug()
     {
-        dc = GetComponent<DiceController>();
-        dc.StopDice = DebugStopHandler;
-        dc.Roll("red");
+        onClickSubject.OnNext(Unit.Default);
     }
-    public void OnClickForDebug2()
-    {
-        _mainVM.SellFood();
-    }
+
     private void DebugStopHandler()
     {
         var dice = Dice.Value("");
         Debug.Log("Stop Dice: " + Dice.Value(""));
-        // _mainVM.AdvanceTime(dice);
-        _mainVM.NotifyDiceCasted(); // 判断待ちステータスに進める
-
-        // 熟成待ち状態になったらダイスの出目で熟成
-        StartCoroutine(ControlTurnCoroutine(dice));
+        onDice.OnNext(dice);
     }
 
-    private void DebugDiceClean()
+    public IObservable<int> DiceRoll()
+    {
+        dc = GetComponent<DiceController>();
+        dc.StopDice = DebugStopHandler;
+        dc.Roll("red");
+
+        return onDice;
+    }
+
+    public IObservable<Unit> OnDiceButtonClickAsObservable
+    {
+        get { return onClickSubject; }
+    }
+    private Subject<UniRx.Unit> onClickSubject = new Subject<Unit>();
+    private Subject<int> onDice = new Subject<int>();
+
+    public void DebugDiceClean()
     {
         Dice.Clear();
         dc.StopDice -= DebugStopHandler;
-    }
-
-    private IEnumerator ControlTurnCoroutine(int dice)
-    {
-        yield return new WaitUntil(() =>
-        {
-            return Status == MainModel.Status.Event; // イベントカードオープン待ちになったら
-        });
-
-        bool EventChecked = false;
-        // イベントカードオープン
-        _mainVM.EventCardOpen();
-        GetComponent<EventCardController>().DrawEventCard(
-            () =>
-            {
-                DebugDiceClean();
-                EventChecked = true;
-            });
-
-        yield return new WaitUntil(() =>
-        {
-            return EventChecked && Status == MainModel.Status.Aging; // 熟成待ち状態になったら
-        });
-
-        // 熟成
-        _mainVM.AdvanceTime(dice);
-
-        yield return new WaitUntil(() =>
-        {
-            return EventChecked && Status == MainModel.Status.NextTurn; // 次のターンへの移行待ち状態になったら
-        });
-
-        // ２秒待って
-        yield return new WaitForSeconds(2.0f);
-        _mainVM.GoNextTurn(); // 次のターンへ移行
     }
 }
--- TimeIsDelicious/Assets/mvrp/MainPresenter.cs
+++ TimeIsDelicious/Assets/mvrp/MainPresenter.cs
@@ -6,6 +6,7 @@ using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using RuleManager;
 using System.Linq;
+using System;
 
 public class MainPresenter : MonoBehaviour {
 
@@ -15,6 +16,15 @@ public class MainPresenter : MonoBehaviour {
 
     [SerializeField]
     private EventCardController eventCardController;
+
+    [SerializeField]
+    private GameDirector gameDirector;
+
+    [SerializeField]
+    private PopupMessaegController popupMessageController;
+
+    [SerializeField]
+    private PlayersUIWindowController playerWindowController;
 
     private MainModel _singletonMainModel;
 
@@ -53,5 +63,131 @@ public class MainPresenter : MonoBehaviour {
                 card.Weather[RuleManager.EventType.Humid],
                 card.Weather[RuleManager.EventType.Wind]);
         });
-	}
+
+      
+        _singletonMainModel.CurrentStatus.Subscribe(status =>
+        {
+            gameDirector.Status = status;
+            if (status == MainModel.Status.WaitForRoundStart)
+            {
+                StartCoroutine(RoundStart());
+            }
+        });
+
+        _singletonMainModel.CurrentPlayer.Subscribe(player =>
+        {
+            if (player == null)
+            {
+                return;
+            }
+
+            if (currentPlayerBetsDisposable != null)
+            {
+                currentPlayerBetsDisposable.Dispose();
+            }
+
+            currentPlayerBetsDisposable = player.Bets.ObserveCountChanged(true).Subscribe(cnt =>
+            {
+                if (_singletonMainModel.CurrentStatus.Value == MainModel.Status.DecisionMaking)
+                {
+                    // 売るorパス決定ステータスに入ったとき、カレントプレイヤが肉をもってなかったら自動パス
+                    if (cnt == 0)
+                    {
+                        MainModel.Instance.Pass();
+                    }
+                }
+            });
+
+            gameDirector.CurrentPlayerName = player.Name;
+            if (_singletonMainModel.CurrentStatus.Value == MainModel.Status.Betting)
+            {
+                popupMessageController.Popup(player.Name + "さんは肉を選んでください。");
+            }
+            if (_singletonMainModel.CurrentStatus.Value == MainModel.Status.DecisionMaking)
+            {
+                // 売るorパス決定ステータスに入ったとき、カレントプレイヤが肉をもってなかったら自動パス
+                if (player.Bets.Count == 0)
+                {
+                    MainModel.Instance.Pass();
+                }
+            }
+        });
+
+        _singletonMainModel.TurnCount.Subscribe(cnt => {
+            gameDirector.TurnCount = cnt;
+        });
+
+        _singletonMainModel.RoundCount.Subscribe(cnt =>
+        {
+            gameDirector.RoundCount = cnt;
+        });
+
+        gameDirector.OnDiceButtonClickAsObservable.Subscribe(_=>
+        {
+            gameDirector.DiceRoll().Subscribe(dice=>
+            {
+
+                MainModel.Instance.NotifyDiceCasted(); // 判断待ちステータスに進める
+
+                // 熟成待ち状態になったらダイスの出目で熟成
+                StartCoroutine(ControlTurnCoroutine(dice));
+            });
+        });
+
+        var pObj = GameObject.Find("PermanentObj")?.GetComponent<PermanentObj>();
+        int? numOfPlayers = pObj?.playerNum;
+        _singletonMainModel.StartTimeIsDelicious(numOfPlayers.HasValue ? numOfPlayers.Value : 4);
+    }
+	
+    private IDisposable currentPlayerBetsDisposable;
+
+    private IEnumerator RoundStart()
+    {
+        // PlyerUIWindowが準備完了したら
+        yield return new WaitUntil(() =>
+        {
+            return playerWindowController.UIRready;
+        });
+
+        // メッセージを表示して、確認されたらStartRound
+        popupMessageController.Popup("ラウンド開始します", () =>
+        {
+            _singletonMainModel.StartTimeIsDeliciousRound();
+        });
+    }
+
+    private IEnumerator ControlTurnCoroutine(int dice)
+    {
+        yield return new WaitUntil(() =>
+        {
+            return _singletonMainModel.CurrentStatus.Value == MainModel.Status.Event; // イベントカードオープン待ちになったら
+        });
+
+        bool EventChecked = false;
+        // イベントカードオープン
+        _singletonMainModel.EventCardOpen();
+        eventCardController.DrawEventCard(
+            () =>
+            {
+                gameDirector.DebugDiceClean();
+                EventChecked = true;
+            });
+
+        yield return new WaitUntil(() =>
+        {
+            return EventChecked && _singletonMainModel.CurrentStatus.Value == MainModel.Status.Aging; // 熟成待ち状態になったら
+        });
+
+        // 熟成
+        _singletonMainModel.AdvanceTime(dice);
+
+        yield return new WaitUntil(() =>
+        {
+            return EventChecked && _singletonMainModel.CurrentStatus.Value == MainModel.Status.NextTurn; // 次のターンへの移行待ち状態になったら
+        });
+
+        // ２秒待って
+        yield return new WaitForSeconds(2.0f);
+        _singletonMainModel.GoNextTurn(); // 次のターンへ移行
+    }
 }
